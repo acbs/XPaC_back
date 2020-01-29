12bfcf620dfe719a6eb17159f456dd4212b8394e
Silva <andre.carlos.b.silva@accenture.com>
2020-01-29 08:37:41 -0300

    Commit inicial

---
+++ src/main/java/com/xpacback/XPaCBackApplication.java
@@ -0,0 +1,13 @@
+package com.xpacback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XPaCBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XPaCBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xpacback/XPaCBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpacback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XPaCBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
